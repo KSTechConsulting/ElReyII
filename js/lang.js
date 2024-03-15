b2e7214abb60ff4e99238c32cc15edad4fddd96e
joselora <jsloravargas@gmail.com>
2024-03-15 15:04:56 -0400

    Changing some translated contact page text.

--- js/lang.js
+++ js/lang.js
@@ -12,18 +12,48 @@ function changeLanguage(lang) {
     // For the sake of simplicity, we'll use a simple object
     console.log(lang);
     const languageData = {
-    'en': { 
+    'en': {
+       //header
+
         home: 'Home',
         menu: 'Menu',
         contact: 'Contact',
         language: 'Language',
+
+        //body
+
+        homecontactbd:'Home', 
+        pagescontactbd: 'Pages',
+        contactus: 'Contact Us',
+        contactbd: 'Contact',
+
+    //Contact Start -->
+        contactst1: 'Contact Us',
+        contactst2: 'Contact For Any Query',
+       
+
     
     },
-    'es': { 
+    'es': {
+        //header
+
         home: 'Inicio',
         menu: 'Menú',
         contact: 'Contacto',
         language: 'Idioma',
+
+       //body
+       homecontactbd: 'Inicio', 
+       pagescontactbd: 'Páginas',
+       contactus: 'Contacta con nosotros',
+       contactbd: 'Contacto',
+
+        //Contact Start -->
+        contactst1: 'Contacta con nosotros',
+        contactst2: 'Contacto Para Cualquier Consulta',
+
+
+
     }
         // Add more languages as needed
     };
@@ -34,6 +64,15 @@ function changeLanguage(lang) {
     $('#menu').text(languageData[lang].menu);
     $('#contact').text(languageData[lang].contact);
     $('#language').text(languageData[lang].language);
+
+    $('#homecontactbd').text(languageData[lang].homecontactbd);
+    $('#pagescontactbd').text(languageData[lang].pagescontactbd);
+    $('#contactus').text(languageData[lang].contactus);
+    $('#contactbd').text(languageData[lang].contactbd);
+
+    //Contact Start -->
+    $('#contactst1').text(languageData[lang].contactst1);
+    $('#contactst2').text(languageData[lang].contactst2);
 }
 
  // Function to initialize language on page load
