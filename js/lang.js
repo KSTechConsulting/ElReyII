ac5cd158b339a1384a3569e6a5bbda824378b522
joselora <jsloravargas@gmail.com>
2024-03-26 15:25:29 -0400

    Changes contact KSv1

--- js/lang.js
+++ js/lang.js
@@ -10,7 +10,7 @@ function changeLanguage(lang) {
  function updateContent(lang) {
       // Fetch language data dynamically (from server or JSON file)
     // For the sake of simplicity, we'll use a simple object
-    console.log(lang);
+    //console.log(lang);
     const languageData = {
     'en': {
        //header
@@ -30,7 +30,29 @@ function changeLanguage(lang) {
     //Contact Start -->
         contactst1: 'Contact Us',
         contactst2: 'Contact For Any Query',
-       
+        contactbooking: 'Booking',
+        contactgeneral: 'General',
+        contacttechnical: 'Technical',
+        contactbootunsendmessage: 'Send Message',
+        name: 'Your Name',
+        email: 'Your Email',
+        subject: 'Subject',
+        message: 'Leave a message here',
+        namelabel: 'Your Name',
+        emaillabel: 'Your Email',
+        subjectlabel: 'Subject',
+        messagelabel: 'Message',
+
+
+    //footer
+         footernew: 'Send us your email to stay informed about our offers and events',
+         footerbootunnews: 'Newsletter',
+         footerbootunsendnews: 'Send',
+         footeryouremail: 'Your email',
+         footeropening: 'Opening',
+         footercontact: 'Contact',
+         footercompny: 'Company',
+         footercontactus: 'Contact Us', 
 
     
     },
@@ -51,13 +73,38 @@ function changeLanguage(lang) {
         //Contact Start -->
         contactst1: 'Contacta con nosotros',
         contactst2: 'Contacto Para Cualquier Consulta',
+        contactbooking: 'Reserva',
+        contactgeneral: 'General',
+        contacttechnical: 'Técnico',
+        contactbootunsendmessage: 'Enviar mensaje',
+        name: 'Su nombre',
+        email: 'Tu correo electrónico',
+        subject: 'Sujeto',
+        message: 'Deja un mensaje aquí',
+        namelabel: 'Su nombre',
+        emaillabel: 'Tu correo electrónico',
+        subjectlabel: 'Sujeto',
+        messagelabel: 'Mensaje',
+
+        //footer
+         footernew: 'Envianos tu email para mantenerte informado, sobre nuestras ofertas y eventos',
+         footerbootunnews: 'Boletin informativo',
+         footerbootunsendnews: 'Enviar',
+         footeryouremail: 'Tu correo electrónico',
+         footeropening: 'Apertura',
+         footercontact: 'Contacto',
+         footercompny: 'Compañía',
+         footercontactus: 'Contacta con nosotros',
 
 
 
     }
         // Add more languages as needed
     };
-
+    //var idiomaSeleccionado = document.getElementById('selectorIdioma').value;
+    document.getElementById('name').setAttribute('placeholder', languageData[lang].name);
+    document.getElementById('footeryouremail').setAttribute('placeholder', languageData[lang].email);
+   
     // Update content based on selected language
    
     $('#home').text(languageData[lang].home);
@@ -73,6 +120,25 @@ function changeLanguage(lang) {
     //Contact Start -->
     $('#contactst1').text(languageData[lang].contactst1);
     $('#contactst2').text(languageData[lang].contactst2);
+    $('#contactbooking').text(languageData[lang].contactbooking);
+    $('#contactgeneral').text(languageData[lang].contactgeneral);
+    $('#contacttechnical').text(languageData[lang].contacttechnical);
+    $('#contactbootunsendmessage').text(languageData[lang].contactbootunsendmessage);
+    $('#namelabel').text(languageData[lang].name);
+    $('#emaillabel').text(languageData[lang].email);
+    $('#subjectlabel').text(languageData[lang].subject);
+    $('#messagelabel').text(languageData[lang].message);
+
+    //footer
+    $('#footernew').text(languageData[lang].footernew);
+    $('#footerbootunnews').text(languageData[lang].footerbootunnews);
+    $('#footerbootunsendnews').text(languageData[lang].footerbootunsendnews);
+    $('#footeryouremail').text(languageData[lang].footeryouremail);
+    $('#footeropening').text(languageData[lang].footeropening);
+    $('#footercontact').text(languageData[lang].footercontact);
+    $('#footercompny').text(languageData[lang].footercompny);
+    $('#footercontactus').text(languageData[lang].footercontactus);
+
 }
 
  // Function to initialize language on page load
